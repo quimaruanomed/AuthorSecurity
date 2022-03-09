0e2a8390558944003e9cbbdceaa6a538dcdad5c7
Quima Ruano <jruanomed@gmail.com>
2022-03-09 11:53:48 +0100

    Initial commit

---
+++ src/main/java/AuthorSecurity/AuthorSecurity/AuthorSecurityApplication.java
@@ -0,0 +1,13 @@
+package AuthorSecurity.AuthorSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AuthorSecurity/AuthorSecurity/AuthorSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package AuthorSecurity.AuthorSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
